4f6633dcd735c88ea62e218a00ce3c2c0cc76159
Florian Wenzel <florian.wenzel@susiandjames.com>
2019-12-16 12:11:52 +0100

    code refactoring

--- bot.py
+++ bot.py
@@ -1,21 +1,40 @@
 import praw
-from rb import onlyLink
-import credentials
+import re
+from credentials import *
 
-reddit = praw.Reddit(client_id=credentials.client_id,
-                     client_secret=credentials.client_secret,
-                     password=credentials.password,
-                     user_agent=credentials.user_agent,
-                     username=credentials.username)
 
-print(reddit.user.me())
+def onlylink(submission):
+    if submission.is_self and not submission.approved:
+        content = submission.selftext
+        regexhttp = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+        regexwww = 'www.(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+        urls = re.findall(regexhttp, submission.selftext)
+        urls.extend(re.findall(regexwww, content))
 
-subreddit = reddit.subreddit('rockharz')
-for submission in subreddit.stream.submissions():
-    if submission.is_self:
-        if onlyLink(submission.selftext):
-            if not submission.approved:
-                removal_message = "Dieser Post wird entfernt, da er ein räudiger selfpost mit nur einem link ist.   " \
-                                 "\nSchäm dich! "
-                submission.mod.remove()
-                submission.mod.send_removal_message(removal_message, title='depp', type='public')
+        # remove all found urls, whitespaces and linebreaks
+        for url in urls:
+            content = content.replace(url, "")
+        content = content.replace(" ", "")
+        content = content.replace("\n", "")
+
+        if not content:
+            removal_message = "Dieser Post wird entfernt, da er ein räudiger selfpost mit nur einem link ist.   " \
+                              "\nSchäm dich! "
+            submission.mod.remove()
+            submission.mod.send_removal_message(removal_message, title='depp', type='public')
+
+
+if __name__ == "__main__":
+
+    # credentials are imported from credentials.py
+    reddit = praw.Reddit(client_id=client_id,
+                         client_secret=client_secret,
+                         password=password,
+                         user_agent=user_agent,
+                         username=username)
+
+    print(reddit.user.me())
+
+    subreddit = reddit.subreddit('rockharz')
+    for submission in subreddit.stream.submissions():
+        onlylink(submission)
