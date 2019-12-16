2bfb427929df138702bb8f895f9ebd5ffa93d044
Florian Wenzel <florian.wenzel@susiandjames.com>
2019-12-16 09:33:06 +0100

    remove umlauts

--- bot.py
+++ bot.py
@@ -15,11 +15,9 @@ for submission in subreddit.stream.submissions():
     if submission.is_self:
         if onlyLink(submission.selftext):
             if not submission.approved:
-                removal_message = "Dieser Post wird entfernt, da er ein räudiger selfpost mit nur einem link ist.   " \
-                                  "\nSchäm dich! "
+                removal_message = "Dieser Post wird entfernt, da er ein raeudiger selfpost mit nur einem link ist.   " \
+                                  "\nSchaem dich! "
                 # comment = submission.reply(removal_message)
                 # comment.mod.distinguish(how='yes', sticky=True)
-                submission.mod.send_removal_message("Dieser Post wird entfernt, da er ein räudiger selfpost mit nur "
-                                                    "einem link ist.   \nSchäm dich!", title="Beitrag GELÖSCHT",
-                                                    type="private_exposed")
+                submission.mod.send_removal_message(removal_message, title="Beitrag GELÖSCHT", type="private_exposed")
                 submission.mod.remove()
