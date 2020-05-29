4a261f77ca98ef0ec63b6b5db2642675d086e756
Florian Wenzel <florian.wenzel@susiandjames.com>
2020-05-30 01:06:26 +0200

    changed mayor things
    works now with the removal of posts
    edits sidebar

--- bot.py
+++ bot.py
@@ -31,7 +31,7 @@ def count(subreddit):
     need_approval = []
     undecided = []
 
-    for submission in subreddit.hot(limit=1000):
+    for submission in subreddit.hot(limit=2500):
         if submission.stickied:
             sticky = submission
         else:
@@ -45,36 +45,69 @@ def count(subreddit):
             else:
                 undecided.append(submission)
 
-    percentage = len(translated)/len(all_strings)
-    message = "Hey! There are "+str(len(all_strings))+" strings submitted already! \n" \
-            "We already translated "+str(len(translated))+" of them! (this is "+str((percentage*100))+"%!) \n" \
-            "There are "+ str((len(all_strings)-len(translated))) +" strings to translate, where "+ str(len(undecided)) +" of them are undecided and " \
-            + str(len(need_approval)) +" need an urgent decision!\n"
+
+    removed = 0
+    for log in subreddit.mod.log(limit=10000):
+        if log.action == "removelink":
+            removed += 1
+
+    all_string_count = len(all_strings)+removed
+    translated_count = len(translated)+removed
+    undecided_count = len(undecided)
+    need_approval_count = len(need_approval)
+
+    a = str(translated_count)+"/"+str(all_string_count)
+    b = str(round( (translated_count) / (all_string_count)*100,2))
+    c = str(all_string_count - translated_count)
+    d = str(undecided_count)
+    e = str(need_approval_count)
+
     
-    comment_exists = False
-    for comment in sticky.comments:
-        if comment.author.name == "botzei":
-            comment.edit(message)
-            comment_exists = True
-    if not comment_exists:    
-        sticky.reply(message)
-    print(message)
+    message = "Hey! There are "+str(all_string_count)+" strings submitted already! \n" \
+            "We already translated "+str(translated_count)+" of them! (this is "+b+"%!) \n" \
+            "There are "+ c+" strings to translate, where "+ d +" of them are undecided and " \
+            + e +" need an urgent decision!\n"
+    print(message) # for quick info/debugging in console
+
+    # For a comment in the sticky submission
+    # comment_exists = False
+    # for comment in sticky.comments:
+    #     if comment.author.name == "botzei":
+    #         comment.edit(message)
+    #         comment_exists = True
+    # if not comment_exists:    
+    #     sticky.reply(message)
+
+
+    # this edits the sidebar
+    sidebar_text = "__"+a+"__\n\n" \
+            + "__"+b+"%__ translated\n\n" \
+            + "__"+c+"__ open \n\n" \
+            + "__"+d+"__ undecided\n\n" \
+            + "__"+e+"__ need urgent decision\n\n" \
+            + "[Konsistenztabelle] (https://docs.google.com/spreadsheets/d/1ez07F6jysWb7pAKo5gNzCjegAclD3RCWZdNAEy_TL4U/edit?usp=sharing)"
+    
+    sidebar = subreddit.wiki['config/sidebar']
+    
+    # edits sidebar only if somthings changed
+    if sidebar.content_md != sidebar_text:
+        sidebar.edit(sidebar_text)
+
 
 if __name__ == "__main__":
 
     # credentials are imported from credentials.py
-    try:
-        twoFA = "788459"
-        reddit = praw.Reddit(client_id=client_id,
-                            client_secret=client_secret,
-                            password=password+":"+twoFA,
-                            user_agent=user_agent,
-                            username=username)
-    except:
-        print("error")
+    reddit = praw.Reddit(client_id=client_id,
+                        client_secret=client_secret,
+                        password=password,
+                        user_agent=user_agent,
+                        username=username)
+
     print("Login successful")
 
     subreddit = reddit.subreddit('translation_german')
     count(subreddit)
+
+
     # for submission in subreddit.stream.submissions():
     #     onlylink(submission)
